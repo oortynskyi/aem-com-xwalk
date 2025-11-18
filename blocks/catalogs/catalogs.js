a0277606af3e0deb97d7a526d6c417c2dd79f46e
Oleh Ortynskyi <oortynskyi@sii.pl>
2025-11-18 14:14:25 +0100

    update catalogs block

--- blocks/catalogs/catalogs.js
+++ blocks/catalogs/catalogs.js
@@ -3,18 +3,27 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
+
     const rowLink = row.querySelector('a');
+i
     while (row.firstElementChild) li.append(row.firstElementChild);
+
+    if (rowLink) {
+      const linkContainer = rowLink.closest('div');
+      if (linkContainer) linkContainer.remove();
+    }
+
     if (rowLink) {
       const footer = document.createElement('div');
       footer.className = 'catalogs-catalog-footer';
-      const linkClone = rowLink.cloneNode(true);
-      footer.append(linkClone);
+      footer.append(rowLink.cloneNode(true));
       li.append(footer);
     }
+
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
         div.className = 'catalogs-catalog-image';
@@ -22,12 +31,15 @@ export default function decorate(block) {
         div.className = 'catalogs-catalog-body';
       }
     });
+
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.replaceChildren(ul);
 }
